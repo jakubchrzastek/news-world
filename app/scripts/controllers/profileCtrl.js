4ee16b20b1818f2833a23da9eaff6785b3c31715
Jakub Chrzastek <j.chrzastek@gmail.com>
2016-05-14 12:42:06 +0200

    add user email/password change ctrl

--- app/scripts/controllers/profileCtrl.js
+++ app/scripts/controllers/profileCtrl.js
@@ -2,22 +2,63 @@
 
 angular.module('newsApp')
 	.controller('profileCtrl', ['$http', '$scope', function($http, $scope){
-		$scope.sendEmail = function(email){
-			$http.put('http://news-world.iiar.pwr.edu.pl/api/v1/users/me/',
-			{
-	    		user: {
-	        email: email
-	    		}
-			},
-			
+
+		$http.get('http://news-world.iiar.pwr.edu.pl/api/v1/users/me/',
 			{
 	        	headers: {
-	            Authorization: 'Token ' + localStorage.getItem('token')
+	        Authorization: 'Token ' + localStorage.getItem('token')
 	        	}
+		}).success(function(response){
+			$scope.userData = response.users;
+			localStorage.setItem('email', response.user.email);
+		});
+
+		$scope.editEmail = function(new_email){
+			if(localStorage.getItem('email')!==new_email){
+				$http.put('http://news-world.iiar.pwr.edu.pl/api/v1/users/me/',
+					{
+			    		user: {
+					        email: new_email
+					    }
+					},
+					
+					{
+			        	headers: {
+			        		Authorization: 'Token ' + localStorage.getItem('token')
+			        	}
+				}).success(function(){
+					swal("Success!", "You successfull change your email adress", "success");
+				}).error(function(){
+					swal("Some Error!", "You need fix your mistake", "error");
+				});
+			} 
+			else{
+				swal("Error!", "You choose the same email!" , "error");
+			};
+			
+		};
+
+		$scope.editPassword = function(current, new_password, new_passwordRepeat){
+			if(current!==new_password){
+				$http.post('http://news-world.iiar.pwr.wroc.pl/api/v1/users/me/password/',
+					{
+						user: {
+							original_password: current,
+							new_password: new_password, new_passwordRepeat
+						}
+					},
+					{
+						headers: {
+							Authorization: 'Token ' + localStorage.getItem('token')
+						}
+				}).success(function(){
+					swal("Success!", "You successfull change your password", "success");
+				}).error(function(){
+					swal("Some Error!", "Problem with communication to the server", "error");
+				});
 			}
-		).success(function(){
-			swal("Success!", "You successfull change your email adress", "success");
-		}).error(function(){
-			swal("Some Error!", "You need fix your mistake", "error");
-		})};
+			else{
+				swal("Error!", "You choose the same password!" , "error");
+			};
+		};
 	}]);
